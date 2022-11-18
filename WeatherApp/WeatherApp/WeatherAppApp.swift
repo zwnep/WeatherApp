95e422cec44251967b462e260ec5b2a052b710af
zeynep demirtaş <71128703+zwnep@users.noreply.github.com>
2022-11-19 02:45:04 +0300

    Add files via upload

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Zeynep Demirtaş on 19.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
